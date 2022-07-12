6ba2f68293a8b0638a91348aaac968e3921d12d2
myhearisranch <87411323+myhearisranch@users.noreply.github.com>
2022-07-13 06:25:42 +0900

    要素が10個でなくても対応するように修正しました。

--- sort.rb
+++ sort.rb
@@ -1,15 +1,11 @@
 array = [10, 8, 3, 5, 2, 4, 11, 18, 20, 33]
-
+number = array.size
 x=0
 
-while x < 10 do
-    for i in 0..8
+while x < number do
+    for i in 0..number-2
         if array[i] < array[i+1]
-            a = array[i] 
-            b = array[i+1]
-            a,b = b,a
-            array[i] = a
-            array[i+1] = b 
+            array[i] , array[i+1] = array[i+1], array[i]
         end
     end
     x += 1
